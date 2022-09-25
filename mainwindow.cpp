7fde688ec88118c45c50e87a859c75a9d19b08fc
BartG37 <bgronicki@wp.pl>
2022-09-25 02:21:34 +0200

    mainwndows 28

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,7 +9,7 @@ MainWindow::MainWindow(ser* ser1, model* model1, QWidget *parent)
 {
     qDebug() << "MainWindow-konstruktor: ";
     ui->setupUi(this);
-    QVector<bg*> TagItem(27,nullptr);
+    QVector<bg*> TagItem(28,nullptr);
     qDebug() << TagItem.size();
     setTagItem();
     setTagItem_cfg();
